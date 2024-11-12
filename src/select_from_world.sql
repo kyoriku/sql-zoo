6a529332f140e07053e4875731f6aa1900db9d32
Austin Graham <austiingraham@gmail.com>
2024-11-12 18:41:13 -0500

    update select_from_world.sql

--- src/select_from_world.sql
+++ src/select_from_world.sql
@@ -1,3 +1,5 @@
+-- https://sqlzoo.net/wiki/SELECT_from_WORLD_Tutorial
+
 -- 1. Read the notes about this table (https://sqlzoo.net/wiki/Read_the_notes_about_this_table.). Observe the result of running this SQL command to show the name, continent and population of all countries.
 SELECT name, continent, population
   FROM world
